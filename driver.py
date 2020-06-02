d9c086173f36edea13a442853ce2eda339432c1d
hans <hansschumann24@gmail.com>
2020-06-01 21:05:25 -0700

    First commit to the repo. A simple script to print some tweets pulled at that moment.

---
+++ driver.py
@@ -0,0 +1,22 @@
+"""
+This is currently the main script for pulling tweets until this project is cleaned up and has a direction.
+Right now this script does nothing except pull some tweets based on simple criteria.
+"""
+
+# imports external packages
+import GetOldTweets3 as got
+import pandas as pd
+
+search_words = "#blm"
+date_since = "2020-05-23"
+
+
+tweetCriteria = got.manager.TweetCriteria().setQuerySearch('#BLM').setMaxTweets(3)
+tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+for tweet in tweets:
+    print(tweet.text + '\n')
+
+print(tweets[0].__dict__)
+
+
+
